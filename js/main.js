fc11aab88342139a0b2b7c23e0ff6384bd5dcf16
Joe Richardson <joe@lifes.gd>
2017-08-17 21:47:36 +0100

    feat(update)

--- js/main.js
+++ js/main.js
@@ -1,99 +1,22 @@
-(function(window, document, $, undefined) {
+var textarea = document.querySelector('#textarea');
+var filename = document.querySelector('#input-name');
+var save     = document.querySelector('.js-save');
 
-  'use strict';
+function saveFile() {
+  var blob = new Blob([textarea.value], {type: "text/plain;charset=utf-8"});
+  saveAs(blob, filename.value + ".txt");
+}
 
-  // Variables
-  var $trigger  = $('.js-trigger');
-  var $count    = $('.js-count');
-  var $div      = $('.site-footer__meta');
-
-  var menu      = '.js-menu';
-  var footer    = '.js-footer';
-  var toggleBtn = 'site-footer__count--toggle'
-
-  // Counter
-  var $chars    = $('.counter__chars');
-  var $words    = $('.counter__words');
-  var $lines    = $('.counter__lines');
-
-  // Toggle info box
-  function menuBox() {
-    $(menu).slideToggle();
-  }
-
-  function toggleFooter() {
-    $(footer).slideToggle();
-  }
-
-  var counter = function() {
-      var value = $('textarea').val();
-
-      if (value.length == 0) {
-          $chars.html(0);
-          $words.html(0);
-          $lines.html(0);
-          return;
-      }
-
-      var regex = /\s+/gi;
-      var wordCount = value.trim().replace(regex, ' ').split(' ').length;
-      var charCount = value.trim().length;
-      var lineCount = value.split(/\r*\n/).length
-
-      $words.html(wordCount);
-      $chars.html(charCount);
-      $lines.html(lineCount);
-  };
-
-  $(document).ready(function() {
-    $('.counter').click(counter);
-    $('textarea').change(counter);
-    $('textarea').keydown(counter);
-    $('textarea').keypress(counter);
-    $('textarea').keyup(counter);
-    $('textarea').blur(counter);
-    $('textarea').focus(counter);
-  });
-
-  $trigger.on('click', function() {
-    menuBox();
-  });
-
-  $count.on('click', function() {
-    $(this).toggleClass(toggleBtn);
-    toggleFooter();
-  });
-
-  $(window).resize(function () {
-    if ($(window).width() > 688) {
-        $(footer).show();
-    }
-
-    if ($(window).width() < 687) {
-        $(footer).hide();
-    }
-  });
-
-  $('.spin').show().delay(800).fadeOut();
-
-  $('.btn--save').on('click', function () {
-    $('.msg').slideDown();
-  });
-
-  $('.msg__close').on('click', function () {
-    $('.msg').slideUp();
-  });
+save.addEventListener('click', saveFile);
 
 
-    function init() {
-        if (localStorage["message"]) {
-            $('#inputTextArea').val(localStorage["message"]);
-        }
-    }
 
-    init();
+var area = textarea;
 
-    $('.stored').keyup(function () {
-        localStorage[$(this).attr('name')] = $(this).val();
-    });
-})(window, document, jQuery);
+if (!area.value) {
+  area.value = window.localStorage.getItem('value');
+}
+
+textarea.addEventListener('keyup', function() {
+  window.localStorage.setItem('value', area.value);
+}, false);
